3968994f0a0f2f417c09951b292d18c66bedcbe6
Lmorgado13 <Lois_morgado@hotmail.com>
2018-07-08 00:51:55 -0500

    Agregar Sass y Gulp.js

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+(function($) {
+    var $modal = $('#exampleModal');
+    var $email = $('#exampleInputEmail1');
+  
+    $modal.on('shown.bs.modal', function() {
+      $email.trigger('focus');
+    });
+  
+    $('.js-save-changes').on('click', function() {
+  
+      if ($email.val().trim().length > 0) {
+        sendChanges().then(function(value) {
+          $modal.modal('hide');
+        });
+      } else {
+        $modal.find('form').addClass('was-validated');
+      }
+    });
+  
+    function sendChanges() {
+      console.log('>> Saving changes...');
+  
+      return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+          console.log('-- Changes were saved...');
+          resolve();
+        }, 2000);
+      });
+    }
+  })(jQuery);
